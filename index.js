286d62ae553e2d7b0cd4a2992def6bc1ba57c957
Thiago Ferreira <50066360+spherin@users.noreply.github.com>
2023-07-22 15:37:28 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+
+const colors = ['Red', 'Blue', 'Yellow', 'Purple', 'Green']
+const btn = document.getElementById('btn')
+const color = document.querySelector('.color')
+
+btn.addEventListener('click', function(){
+    const randomColor = getRandomColor()
+    document.body.style.background = colors[randomColor]
+    color.textContent = colors[randomColor]
+})
+
+function returnColor(){
+    document.getElementById('color').innerHTML = colors[randomColor]
+}
+
+function getRandomColor(){
+    return Math.floor(Math.random() * colors.length)
+}
